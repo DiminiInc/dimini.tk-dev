95f8712ed6738e44ad7d8911bb417a7f76c369af
Yaskovich Dmitry <kadavr95@outlook.com>
2015-12-24 23:12:00 +0300

    version 0.0.2.0 alpha
    added news section
    added some news posts from our old blog and placeholders for posts pages
    added page with information about company
    added content for Snake page
    added page and its content for Dimini Search
    added page-placeholder for pages under development
    added list of info sources
    obsolete files removed

--- site_files/javascript.js
+++ site_files/javascript.js
@@ -1,3 +1,19 @@
 function hideNotification(id) {
     document.getElementById(id).style.display = 'none';
 }
+
+function magnifyObject(id){
+	if(document.getElementById(id).style.height==="600px")
+	{
+		document.getElementById(id).style.height= 'auto';
+	 	 document.getElementById(id).style.width= "30%";
+	 	 document.getElementById(id).title="Нажмите для увеличения";
+	}
+	else
+	{
+	 document.getElementById(id).style.height= "600px";
+	 	 document.getElementById(id).style.width= "600px";
+	 	 document.getElementById(id).scrollIntoView(true);
+	 	 document.getElementById(id).title="Нажмите для уменьшения";
+	 	}
+}
