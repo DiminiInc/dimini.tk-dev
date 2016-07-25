854c46a4a880748dc4fc349efa8c5d2feffd6204
Yaskovich Dmitry <kadavr95@outlook.com>
2016-07-25 14:47:00 +0300

    version 1.0.3.0 beta
    main page modified to include header using php

--- index.php
+++ index.php
@@ -22,35 +22,7 @@
 				<div id="information">Вы используете тестовую версию сайта. Часть функций и контента может быть недоступна. Открыть старую версию сайта? </div>
 				<div id="information-choice"><a href="http://dimini.tk">Да</a> <a href="#" onclick="hideNotification('notification'); return false;">Нет</a></div>
 			</div>
-			<div id="header">
-				<div id="main_menu">
-					<ul id="mainmenu">
-						<li><a href="./"><img src="./site_files/dimini_icon.png"/></a></li>
-						<li>
-							<a href="./news">Новости</a>
-						</li>
-						<li>
-							<a href="./software">Программы</a>
-							<ul>
-								<li><a href="./software/snake">Змейка</a></li>
-								<li><a href="./software/disearch">Dimini Search</a></li>		
-							</ul>
-						</li>
-						<li>
-							<a href="./testdrive">Тестирование</a>
-							<ul class="submenu">
-								<li><a href="./testdrive/browsers">Сравнение браузеров</a></li>
-								<!-- <li><a href="main.html">Сравнение смартфонов</a></li>
-								<li><a href="main.html">Сравнение поисковых систем</a></li>
-								<li><a href="main.html">Сравнение социальных сетей</a></li>	 -->
-							</ul>
-						</li>
-						<li>
-							<a href="./about_us">О Нас</a>
-						</li>
-					</ul>
-				</div>
-			</div>
+			<?php include("main_menu.php"); ?>
 			<div id="site_content">
 				<p>
 					Dimini - молодая, динамично развивающаяся компания, образованная в 2009 году.
---
+++ main_menu.php
@@ -0,0 +1,29 @@
+<div id="header">
+	<div id="main_menu">
+		<ul id="mainmenu">
+			<li><a href="./"><img src="./site_files/dimini_icon.png"/></a></li>
+			<li>
+				<a href="./news">Новости</a>
+			</li>
+			<li>
+				<a href="./software">Программы</a>
+				<ul>
+					<li><a href="./software/snake">Змейка</a></li>
+					<li><a href="./software/disearch">Dimini Search</a></li>		
+				</ul>
+			</li>
+			<li>
+				<a href="./testdrive">Тестирование</a>
+				<ul class="submenu">
+					<li><a href="./testdrive/browsers">Сравнение браузеров</a></li>
+					<!-- <li><a href="main.html">Сравнение смартфонов</a></li>
+					<li><a href="main.html">Сравнение поисковых систем</a></li>
+					<li><a href="main.html">Сравнение социальных сетей</a></li>	 -->
+				</ul>
+			</li>
+			<li>
+				<a href="./about_us">О Нас</a>
+			</li>
+		</ul>
+	</div>
+</div>
