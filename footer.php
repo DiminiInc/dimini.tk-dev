43865e5b68eba633ca465451978f529a827b0b0c
Yaskovich Dmitry <kadavr95@outlook.com>
2016-07-31 14:09:00 +0300

    version 1.1.0.0 beta
    added php version of footer
    updated php version of header
    some pages were upgraded to php version of footer and new version of header

---
+++ footer.php
@@ -0,0 +1,29 @@
+<style>
+	#footer{
+		text-align:center;
+		background-color:#ddd;
+	}
+
+	#footer li{
+		display:inline;
+	}
+
+	#footer a{
+		text-decoration:none;
+		color:#000;
+		padding-right: 10px;
+		padding-left: 10px;
+	}
+
+	#footer a:hover{
+		text-decoration:underline;
+	}
+</style>
+
+<div id="footer">
+	<ul>
+		<li>© Dimini Inc., 2015</li> 
+		<li><a href="mailto:dimini_inc@inbox.ru">Связаться с нами</a></li>
+		<li><a href="./about_site">О сайте</a></li>
+	</ul>
+</div>
