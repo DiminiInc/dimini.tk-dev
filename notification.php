4668f022e1deca908821210eff461ff766999d86
Yaskovich Dmitry <kadavr95@outlook.com>
2016-08-15 20:05:00 +0300

    version 1.10.7.0 beta
    removed unneeded files
    notifications modified to load using php

---
+++ notification.php
@@ -0,0 +1,10 @@
+<div id="notification">
+			
+				<div id="information">Вы используете тестовую версию сайта. Часть функций и контента может быть недоступна. Открыть старую версию сайта? </div>
+				<div id="information-choice"><a href="http://dimini.tk">Да</a> <a href="#" onclick="localStorage.setItem('notify_close', 'yes');hideNotification('notification'); return false;">Нет</a></div>
+			</div>
+			<div id="language-notification">
+			
+				<div id="information">Looks like you may need to change site language. Do you want to do it?</div>
+				<div id="information-choice"><a href="javascript:void(0)" onclick="showLanguageChoice('language-choice'); return false;">Yes</a> <a href="javascript:void(0)" onclick="replaceLocale();">No</a></div>
+			</div>
