ddc14a9d4c8158097b6aed25ae0d223d2947e8a9
Yaskovich Dmitry <kadavr95@outlook.com>
2019-03-16 19:05:30 +0300

    added hover colors (#38)

--- ru/share-buttons.min.php
+++ ru/share-buttons.min.php
@@ -1,8 +1,8 @@
 <div class="share-buttons">
 <span>Поделиться: </span>
-<a href="javascript:void(0)" onclick='sharePage("vk")' class="nopadding-link" title="Поделиться ВКонтакте"><img src="/global/site-files/vk-logo.svg" alt="ВКонтакте"></a>
-<a href="javascript:void(0)" onclick='sharePage("facebook")' class="nopadding-link" title="Поделиться на Facebook"><img src="/global/site-files/facebook-logo.svg" alt="Facebook"></a>
-<a href="javascript:void(0)" onclick='sharePage("ok")' class="nopadding-link" title="Поделиться в Одноклассниках"><img src="/global/site-files/ok-logo.svg" alt="Одноклассники"></a>
-<a href="javascript:void(0)" onclick='sharePage("twitter")' class="nopadding-link" title="Поделиться в Twitter"><img src="/global/site-files/twitter-logo.svg" alt="Twitter"></a>
-<a href="javascript:void(0)" onclick='sharePage("googleplus")' class="nopadding-link" title="Поделиться в Google+"><img src="/global/site-files/google-plus-logo.svg" alt="Google+"></a>
+<a href="javascript:void(0)" onclick='sharePage("vk")' class="nopadding-link hover-vk" title="Поделиться ВКонтакте"><img src="/global/site-files/vk-logo.svg" alt="ВКонтакте"></a>
+<a href="javascript:void(0)" onclick='sharePage("facebook")' class="nopadding-link hover-facebook" title="Поделиться на Facebook"><img src="/global/site-files/facebook-logo.svg" alt="Facebook"></a>
+<a href="javascript:void(0)" onclick='sharePage("ok")' class="nopadding-link hover-ok" title="Поделиться в Одноклассниках"><img src="/global/site-files/ok-logo.svg" alt="Одноклассники"></a>
+<a href="javascript:void(0)" onclick='sharePage("twitter")' class="nopadding-link hover-twitter" title="Поделиться в Twitter"><img src="/global/site-files/twitter-logo.svg" alt="Twitter"></a>
+<a href="javascript:void(0)" onclick='sharePage("googleplus")' class="nopadding-link hover-google" title="Поделиться в Google+"><img src="/global/site-files/google-plus-logo.svg" alt="Google+"></a>
 </div>
