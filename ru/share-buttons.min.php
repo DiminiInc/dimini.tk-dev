9608f0804caaf026ca344af8c6832ede2b207ba8
Yaskovich Dmitry <kadavr95@outlook.com>
2019-04-02 17:13:34 +0300

    fix typo

--- ru/share-buttons.min.php
+++ ru/share-buttons.min.php
@@ -4,5 +4,5 @@
 <a href="javascript:void(0)" onclick='sharePage("facebook")' class="nopadding-link hover-facebook" title="Поделиться на Facebook"><img src="/global/site-files/facebook-logo.svg" alt="Facebook"></a>
 <a href="javascript:void(0)" onclick='sharePage("ok")' class="nopadding-link hover-ok" title="Поделиться в Одноклассниках"><img src="/global/site-files/ok-logo.svg" alt="Одноклассники"></a>
 <a href="javascript:void(0)" onclick='sharePage("twitter")' class="nopadding-link hover-twitter" title="Поделиться в Twitter"><img src="/global/site-files/twitter-logo.svg" alt="Twitter"></a>
-<a href="javascript:void(0)" onclick='sharePage("email")' class="nopadding-link hover-email" title="Поделить по email"><img src="/global/site-files/email-icon.svg" alt="Email"></a>
+<a href="javascript:void(0)" onclick='sharePage("email")' class="nopadding-link hover-email" title="Поделиться по email"><img src="/global/site-files/email-icon.svg" alt="Email"></a>
 </div>
