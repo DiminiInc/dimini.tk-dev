84f10f4bf2d191442020fe1021b160887f152c36
Yaskovich Dmitry <kadavr95@outlook.com>
2020-10-11 02:01:25 +0300

    Add Lamps to the main page carousel

--- ru/carousel.php
+++ ru/carousel.php
@@ -1,4 +1,5 @@
 <div class="carousel-wrapper" style="height:400px">
+<span class="hidden-target" id="lamps"></span>
 <span class="hidden-target" id="unchained"></span>
 <span class="hidden-target" id="mobile-phones-comparison"></span>
 <span class="hidden-target" id="browsers-comparison"></span>
@@ -8,15 +9,23 @@
 <span class="hidden-target" id="balls"></span>
 <span class="hidden-target" id="dimini-search"></span>
 <span class="hidden-target" id="hearthstone-cards-rating"></span>
-<div class="carousel-item item-1" style="background-color:#fff;background-image:url(/global/site-files/unchained-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-1" style="background-color:#fff;background-image:url(/global/site-files/lamps-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<a class="pagelink" href="/ru/software/lamps/">
+<h2>Lamps</h2>
+<p>Игра, где необходимо пересобрать электроцепь, чтобы добавить большее число ламп, используя ограниченное количество действий</p>
+</a>
+<a class="arrow arrow-prev" href="#hearthstone-cards-rating" title="Перейти на предыдущий"></a>
+<a class="arrow arrow-next" href="#unchained" title="Перейти на следующий"></a>
+</div>
+<div class="carousel-item item-2" style="background-color:#fff;background-image:url(/global/site-files/unchained-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/software/unchained/">
 <h2>Unchained</h2>
 <p>Расширение для браузеров, предназначенное для блокировки отвлекающих веб-сайтов, разделов веб-сайтов и страниц, содержащих определённые слова</p>
 </a>
-<a class="arrow arrow-prev" href="#hearthstone-cards-rating" title="Previous item"></a>
-<a class="arrow arrow-next" href="#mobile-phones-comparison" title="Next item"></a>
+<a class="arrow arrow-prev" href="#lamps" title="Перейти на предыдущий"></a>
+<a class="arrow arrow-next" href="#mobile-phones-comparison" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-2" style="background-color:#fff;background-image:url(/global/site-files/mobile-phones-carousel.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-3" style="background-color:#fff;background-image:url(/global/site-files/mobile-phones-carousel.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/statistics/testdrive/mobile-phones/">
 <h2>Сравнение смартфонов</h2>
 <p>Сравнение множества смартфонов от различных производителей</p>
@@ -24,7 +33,7 @@
 <a class="arrow arrow-prev" href="#unchained" title="Перейти на предыдущий"></a>
 <a class="arrow arrow-next" href="#browsers-comparison" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-3" style="background-color:#fff;background-image:url(/global/site-files/browsers-carousel.png);background-size:100%;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-4" style="background-color:#fff;background-image:url(/global/site-files/browsers-carousel.png);background-size:100%;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/statistics/testdrive/browsers/">
 <h2>Сравнение браузеров</h2>
 <p>Сравнение множества браузеров для различных операционных систем</p>
@@ -32,7 +41,7 @@
 <a class="arrow arrow-prev" href="#mobile-phones-comparison" title="Перейти на предыдущий"></a>
 <a class="arrow arrow-next" href="#snake" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-4" style="background-color:#fff;background-image:url(/global/site-files/snake-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-5" style="background-color:#fff;background-image:url(/global/site-files/snake-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/software/snake">
 <h2>Змейка</h2>
 <p>Популярная игра "Змейка"</p>
@@ -40,7 +49,7 @@
 <a class="arrow arrow-prev" href="#browsers-comparison" title="Перейти на предыдущий"></a>
 <a class="arrow arrow-next" href="#battleship" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-5" style="background-color:#fff;background-image:url(/global/site-files/battleship-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-6" style="background-color:#fff;background-image:url(/global/site-files/battleship-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/software/battleship">
 <h2>Морской бой</h2>
 <p>Популярная игра "Морской бой"</p>
@@ -48,7 +57,7 @@
 <a class="arrow arrow-prev" href="#snake" title="Перейти на предыдущий"></a>
 <a class="arrow arrow-next" href="#game-of-life" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-6" style="background-color:#fff;background-image:url(/global/site-files/game-of-life-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-7" style="background-color:#fff;background-image:url(/global/site-files/game-of-life-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/software/game-of-life">
 <h2>Игра "Жизнь"</h2>
 <p>Клеточный автомат Джона Конвея</p>
@@ -56,7 +65,7 @@
 <a class="arrow arrow-prev" href="#battleship" title="Перейти на предыдущий"></a>
 <a class="arrow arrow-next" href="#balls" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-7" style="background-color:#fff;background-image:url(/global/site-files/balls-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-8" style="background-color:#fff;background-image:url(/global/site-files/balls-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/software/balls">
 <h2>Шары</h2>
 <p>Залипательные столкновения шаров на экране вашего монитора</p>
@@ -64,7 +73,7 @@
 <a class="arrow arrow-prev" href="#game-of-life" title="Перейти на предыдущий"></a>
 <a class="arrow arrow-next" href="#dimini-search" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-8" style="background-color:#fff;background-image:url(/global/site-files/disearch-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-9" style="background-color:#fff;background-image:url(/global/site-files/disearch-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/software/disearch">
 <h2>Dimini Search</h2>
 <p>Программа для быстрого поиска в различных поисковых системах</p>
@@ -72,12 +81,12 @@
 <a class="arrow arrow-prev" href="#balls" title="Перейти на предыдущий"></a>
 <a class="arrow arrow-next" href="#hearthstone-cards-rating" title="Перейти на следующий"></a>
 </div>
-<div class="carousel-item item-9" style="background-color:#fff;background-image:url(/global/site-files/hearthstone-cards-rating-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
+<div class="carousel-item item-10" style="background-color:#fff;background-image:url(/global/site-files/hearthstone-cards-rating-logo.png);background-size:contain;background-repeat:no-repeat;background-position:center">
 <a class="pagelink" href="/ru/statistics/games/hearthstone/crafting-guide/">
 <h2>Рейтинг карт Hearthstone</h2>
 <p>Информация о текущей полезности карт в коллекционной карточной игре Hearthstone</p>
 </a>
 <a class="arrow arrow-prev" href="#dimini-search" title="Перейти на предыдущий"></a>
-<a class="arrow arrow-next" href="#unchained" title="Перейти на следующий"></a>
+<a class="arrow arrow-next" href="#lamps" title="Перейти на следующий"></a>
 </div>
 </div>
