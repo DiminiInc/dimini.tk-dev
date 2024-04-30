51fe31d4fd8e42b16fa0b3f580bcd49a5847b5d3
Dmitry Yaskovich <kadavr95@outlook.com>
2024-04-30 22:32:58 +0300

    Fix problems with Google Tag Manager

--- global/tagmanager.min.php
+++ global/tagmanager.min.php
@@ -1,3 +1,3 @@
 
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TKDW72" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>!function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="//www.googletagmanager.com/gtm.js?id=GTM-TKDW72",m.parentNode.insertBefore(r,m)}(window,document,"script","dataLayer")</script>
+<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-TKDW72' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
+<script>!function(e,t,a,n){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});e=t.getElementsByTagName(a)[0],n=t.createElement(a);n.async=!0,n.src="https://www.googletagmanager.com/gtm.js?id=GTM-TKDW72",e.parentNode.insertBefore(n,e)}(window,document,"script","dataLayer")</script>
